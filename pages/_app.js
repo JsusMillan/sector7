3acd1f3aec1b3ed6e2b46df92839a208a9bf9070
Jesús Millán <jesus.sysadmin@appyweb.es>
2022-11-05 17:56:06 -0400

    add google analityc

--- pages/_app.js
+++ pages/_app.js
@@ -2,21 +2,24 @@ import '../styles/globals.css'
 import Layout from '../components/Layout/Main'
 import { DefaultSeo } from 'next-seo';
 import SEO from '../settings/next-seo.config'
-import script from 'next/script';
+import Script from 'next/script';
 
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-Q6PERZ0QND"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+     <Script strategy="lazyOnload" src={'https://www.googletagmanager.com/gtag/js?id=G-Q6PERZ0QND'} />
 
-  gtag('config', 'G-Q6PERZ0QND');
-</script>
-
+<Script strategy="lazyOnload">
+    {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-Q6PERZ0QND', {
+        page_path: window.location.pathname,
+        });
+    `}
+</Script>
       <Layout>
         <DefaultSeo {...SEO} />
         <Component {...pageProps} />
